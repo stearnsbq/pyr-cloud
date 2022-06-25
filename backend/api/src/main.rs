0200fd507bf8fe215bc0157d35b62fb89a072a56
stearnsbq <stearnsbq@vcu.edu>
2022-06-25 13:48:57 -0400

    ye

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -1,9 +1,9 @@
 pub mod routes;
 pub mod model;
 
+#[macro_use] extern crate diesel;
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate dotenv_codegen;
-#[macro_use] extern crate rocket_contrib;
 
 extern crate dotenv;
 use std::sync::Mutex;
@@ -11,9 +11,9 @@ use std::sync::Mutex;
 
 use routes::login::{login};
 use dotenv::dotenv;
-use rocket_contrib::databases::diesel;
+use rocket_sync_db_pools::database;
 
-#[database("sqlite_logs")]
+#[database("pyr-cloud")]
 pub struct DBPool(diesel::PgConnection);
 
 #[launch]
@@ -21,7 +21,8 @@ fn rocket() -> _ {
     dotenv().ok();  
 
     rocket::build()
+    .attach(DBPool::fairing())
     .mount("/", routes![])
     .mount("/login", routes![login])
-    .attach(DBPool::fairing())
+
 }
